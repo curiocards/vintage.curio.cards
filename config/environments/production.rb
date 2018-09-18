f42ec7ec4caa1a178363d2ef0219d14b9a2fb53d
Travis <travisformayor@users.noreply.github.com>
2018-09-18 16:28:16 -0700

    fixed uglifier to es6 harmony

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,9 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # commented out and replaced with below to deal with es6 compatability error
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
