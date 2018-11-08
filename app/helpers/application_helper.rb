dc554b4d67d28922030acbe19157dea3eaa51fb0
Travis <travisformayor@users.noreply.github.com>
2018-11-08 10:27:43 -0800

    pulled card image filename into a helper that checks for the type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def cardImage(card_id)
+    # image_tag("/img/cards/#{id.to_s}.jpg")
+    img_path = Rails.root.join("public/img/cards/").to_s
+    card_id = card_id.to_s
+
+    if File.exists?("#{img_path}#{card_id}.jpg")
+      return "/img/cards/#{card_id}.jpg"
+    elsif File.exists?("#{img_path}#{card_id}.png")
+      return "/img/cards/#{card_id}.png"
+    elsif File.exists?("#{img_path}#{card_id}.gif")
+      return "/img/cards/#{card_id}.gif"
+    # else
+      # ToDo: return the cards https://ipfs location
+    end
+  end
+    
 end
