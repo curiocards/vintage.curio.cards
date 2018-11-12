58843ce9d5bf2e4e4e8a03c2d547ab52c94fe2bb
Travis <travisformayor@users.noreply.github.com>
2018-11-12 10:14:03 -0800

    add new scopes and use in controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,9 +3,13 @@ class SiteController < ApplicationController
   before_action :set_artist, only: [:artist]
 
   def index
-    @artists = Artist.all
-    @cards = Card.all
-    # binding.pry
+    @artists = Artist.all # passed for the artist-list partial
+    latest = [28,29,30] # set the cards under the featured section
+    @feat = Card.by_card_id_array(latest) # pass just the scope of those cards info
+    # @feat[0].id
+    # binding.pry    
+    # @cards = Card.all
+    
   end
 
   def artist
@@ -20,6 +24,8 @@ class SiteController < ApplicationController
   private
     def set_artist
       @artist = Artist.find_by_username(params[:username])
+      @cards = Card.by_artist_username(params[:username])
+      # binding.pry
     end
 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,13 @@
 class Card < ApplicationRecord
+
+  # some of these might need to be called with .sort
+
+  # examples
+  # scope :latest_since_25, -> {where("id > ?", 25)}
+  # scope :artist_id_4, -> {where(artist_id: 4 )}
+  scope :by_artist_id, -> (id) {where(artist_id: id )}
+  scope :by_card_id_array, -> (id_array) {where(id: id_array)} # example: [21,22,23]
+  scope :by_artist_username, -> (username) {where(artist_id: Artist.where(username: username).pluck(:id)[0])} # redo in pure sql?
+
+  # Card.by_artist("robekworld").present? checks if valid username
 end
