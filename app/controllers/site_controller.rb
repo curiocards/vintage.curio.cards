790948ee632c4b91b739a22bf553da0781d5cd4f
Travis <travisformayor@users.noreply.github.com>
2018-11-20 17:46:00 -0800

    controller to priv defs and max-width artist info

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,26 +1,29 @@
 class SiteController < ApplicationController
 
   before_action :set_artist, only: [:artist]
+  before_action :all_artists, only: [:index] # for the artist-list partial
+  before_action :last_cards, only: [:index] # for the featured cards section
 
   def index
-    @artists = Artist.all # passed for the artist-list partial
-    # latest = [28,29,30] # set the cards under the featured section
-    # @feat = Card.by_card_id_array(latest) # Custom scope from card.rb
-    @last = Card.last(3) # another way to get the most recent. faster
-    # binding.pry    
-    # @cards = Card.all
   end
-
   def artist
   end
-
   def about
   end
-
   def gallery
   end
 
   private
+    def all_artists
+      @artists = Artist.all
+    end
+
+    def last_cards
+      # latest = [28,29,30] # set the cards under the featured section
+      # @feat = Card.by_card_id_array(latest) # Custom scope from card.rb
+      @last = Card.last(3) # another way to get the most recent. faster  
+    end
+
     def set_artist
       # Returns the artist model object by there username
       @artist = Artist.find_by_username(params[:username])
@@ -28,7 +31,6 @@ class SiteController < ApplicationController
       # Custom scope from card.rb takes the artist username, finds artist id, finds all cards associated with it
       # List of cards is then grouped by publish_date, and sorted
       @grouped_cards = Card.by_artist_username(params[:username]).group_by{|card| card.publish_date}.sort 
-
       # binding.pry
     end
 
